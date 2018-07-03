254718e4b4e8759f126336ce16d28199b2bbe6c7
Tommy Butler <tbutler.github@internetalias.net>
2018-07-03 14:18:14 -0500

    initial commit

---
+++ runserver.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [[ "$zehrapass" == '' ]]
+then
+	echo 'Set server password as $zehrapass environment variable and rerun!'
+	exit 255
+fi
+
+runrestart ()
+{
+	killall -v bzfs >/dev/null 2>&1
+	sleep 1
+	killall -v bzfs >/dev/null 2>&1
+	sleep 1
+
+	/usr/local/bin/bzfs \
+	-port 5155 \
+	-h -a 0 0 \
+	-passwd "$zehrapass" \
+	-admsg 'This server is operated by ZEHRA'
+        -admsg 'This server is hosted out of a Dallas-based datacenter.' \
+	-srvmsg 'Authenticated players can /report to admins, /pollban, etc.' \
+	-srvmsg 'ZERO TOLERANCE ON CHEATING! No Cursing! Team-kill is disabled.' \
+	-admsg 'This server is operated by ZEHRA'
+        -admsg 'This server is hosted out of a Dallas-based datacenter.' \
+	-admsg 'Authenticated players can /report to admins, /pollban, etc.' \
+	-admsg 'ZERO-TOLERANCE CHEATING POLICY!  No profanity!  Team-kill is disabled.' \
+	-disableBots \
+	-filterChat \
+	-filterCallsigns \
+	-maxidle $((60*10)) \
+	-publictitle 'BZFlag Soccer is Back!' \
+	-publicaddr 'zehrasoccer.networkspeedy.com' \
+	-publickey '' \
+	-reportfile '/home/bzflag/soccer/reportfile.txt' \
+	-badwords '/home/bzflag/soccer/badwords.txt' \
+	-pidfile '/home/bzflag/soccer/bzfs01.pidfile' \
+	-banfile '/home/bzflag/soccer/banfile.txt' \
+	-groupdb '/home/bzflag/soccer/groups.conf' \
+	-userdb '/home/bzflag/soccer/users.conf' \
+	-sb \
+	-spamtime 10 \
+	-spamwarn 5 \
+	-tkkr 5 \
+	-noTeamKills \
+	-lagwarn 325 \
+	-lagannounce 300 \
+	-lagdrop 3 \
+	-jitterwarn 20 \
+	-jitterdrop 3 \
+	-ddd \
+	-loadplugin chathistory,50 \
+	-world '/home/bzflag/soccer/soccer.bzw' \
+	&& echo 'SOCCER BZFLAG Server Started'
+}
+
+run_with_logging () # send output to syslog
+{
+	runrestart 2>&1 | logger -t bzsoccerserver01 
+}
+
+run () # daemonize bzfs
+{
+	run_with_logging & disown -a
+}
+
+run 
